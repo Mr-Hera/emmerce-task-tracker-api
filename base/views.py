0b08119d62068f64196cb1b9d60168825392dd6c
Mr-Hera <randellhera@gmail.com>
2023-03-14 06:05:07 +0300

    Feat: added rest_framwork, decorators to base app views, added README

--- base/views.py
+++ base/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
 
+@api_view(['GET'])
 def endpoints(request):
     data = ['/tasks', 'tasks/<str:pk>/' ]
-    return JsonResponse(data, safe=False)
+    return Response(data)
 
+@api_view(['GET'])
 def task_list(request):
     data = ['Drifting Event @KICC', 'Doctor appointment' ]
-    return JsonResponse(data, safe=False)
+    return Response(data)
 
+@api_view(['GET'])
 def task_detail(request, pk):
     data = pk
-    return JsonResponse(data, safe=False)
+    return Response(data)
